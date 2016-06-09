c3bec8153ebd4c61009ce746e52e48d308623c62
AnichinDaniil <anichindaniil@gmail.com>
2016-06-10 00:02:07 +0300

    Working with bindings

--- schedule.py
+++ schedule.py
@@ -5,6 +5,7 @@ All necessary table definitions for Easy Week
 """
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import BooleanProperty, ListProperty, NumericProperty, \
     StringProperty
 from kivy.lang import Builder
@@ -17,6 +18,20 @@ ruler_hint = 0.08
 
 class KindaButton(Button):
     pass
+
+
+class LessonHolder(BoxLayout):
+    day = NumericProperty()
+    number = NumericProperty()
+
+    def has_inside(self, lesson_center):
+        result = True
+        print 'Lesson center: ', lesson_center
+        if 2 * abs(self.x - lesson_center[0]) > self.width:
+            result = False
+        if 2 * abs(self.y - lesson_center[1]) > self.height:
+            result = False
+        return False
 
 
 class LessonTable(FloatLayout):
@@ -35,13 +50,23 @@ class LessonTable(FloatLayout):
         for i in range(self.day_num):
             for j in range(self.lesson_num):
                 lesson = self.lesson_set[i][j]
-                lesson.size_hint = ((1 - ruler_hint) / self.day_num,
+                box = LessonHolder()
+                box.size_hint = ((1 - ruler_hint) / self.day_num,
                                     (1 - ruler_hint) / self.lesson_num)
-                lesson.pos_hint = {
+                box.pos_hint = {
                     'x': ruler_hint + i * (1 - ruler_hint) / self.day_num,
                     'y': 1 - ruler_hint - (j + 1) *
                                           (1 - ruler_hint) / self.lesson_num}
-                self.add_widget(lesson)
+
+                # lesson.size_hint = ((1 - ruler_hint) / self.day_num,
+                #                     (1 - ruler_hint) / self.lesson_num)
+                # lesson.pos_hint = {
+                #     'x': ruler_hint + i * (1 - ruler_hint) / self.day_num,
+                #     'y': 1 - ruler_hint - (j + 1) *
+                #                           (1 - ruler_hint) / self.lesson_num}
+                box.add_widget(lesson)
+                self.add_widget(box)
+                lesson.bind(pos=self.move_lesson)
 
         # Filling table with rulers-buttons(labels, in future)
         for i in range(self.day_num):
@@ -71,7 +96,7 @@ class LessonTable(FloatLayout):
         )
         self.add_widget(button)
 
-        Clock.schedule_once(lambda dt: self.set_lesson_pos(), 1)
+        # Clock.schedule_once(lambda dt: self.set_lesson_pos(), 1)
 
     def set_lesson_pos(self):
         for i in range(self.day_num):
@@ -84,6 +109,15 @@ class LessonTable(FloatLayout):
                     self.height * (1 - ruler_hint - (j + 1) *
                                    (1 - ruler_hint) / self.lesson_num)
                 )
+
+    def move_lesson(*args):
+        for i in range(len(args)):
+            print i, args[i]
+
+        # for child in self.children:
+        #     if isinstance(child, LessonHolder):
+        #         if child.has_inside(lesson):
+        #             print child
 
 
 class ScheduleApp(App):
